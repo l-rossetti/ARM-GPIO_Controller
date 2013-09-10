f5d22cc4973f9058f80fe6fe8c34e32ba38bfe5a
ciskoITA <francesco.sarti01@ateneopv.it>
2013-09-10 10:43:26 +0200

    add white line to separate authors from comment

--- src/main/java/com/github/arm_gpio_controller/demo/Main.java
+++ src/main/java/com/github/arm_gpio_controller/demo/Main.java
@@ -11,6 +11,7 @@ import java.io.IOException;
  * @author Rossetti Leonardo, email: leonardo.rossetti5@gmail.com
  * @author Sarti Francesco, email: francescosarti@libero.it
  */
+
 //a simple blinking led DEMO application
 public class Main {
 
